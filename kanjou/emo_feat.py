75e89ff721914408966718a830f612d07df80193
ShoheiOhashi <shohei.ohashi@fujielab.org>
2022-11-08 18:45:06 +0900

    モデルの変更

--- kanjou/emo_feat.py
+++ kanjou/emo_feat.py
@@ -3,9 +3,8 @@
 import cv2
 import numpy as np
 from feat import Detector
-frameWidth = 640
-frameHeight = 480
 
+#モデル指定
 face_model = "retinaface"
 landmark_model = "mobilenet"
 au_model = "rf"
@@ -14,21 +13,30 @@ detector = Detector(
     face_model="retinaface",
     landmark_model="mobilefacenet",
     au_model='svm',
-    emotion_model="resmasknet",
+    emotion_model="fer",
     facepose_model="img2pose",
 )
 
+#ビデオ表示関係
 cap = cv2.VideoCapture(0)
+frameWidth = 640
+frameHeight = 480
+
+#感情リスト
 emolist = ["anger", "disgust", "fear", "happiness", "sadness", "surprise", "neutral"]
 
 while True:
+    #画像取得
     ret, img = cap.read()
     show_img = cv2.resize(img, (frameWidth, frameHeight))
     cv2.imshow('Video', show_img)
     img = np.expand_dims(img,0)
+
+    #特徴抽出，感情推定
     faces = detector.detect_faces(img)
     landmarks = detector.detect_landmarks(img, faces)
-    emo_pred = detector.detect_emotions(frame=img, facebox=faces, landmarks=landmarks)
+    emo_pred = detector.emotion_model.detect_emo(img, landmarks)
     
+    #結果表示
     pred_index = np.argmax(emo_pred[0])
     print(emolist[pred_index])
