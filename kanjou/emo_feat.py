8e3c0c802d116338eb42ec77d3473131e5edb050
ShoheiOhashi <shohei.ohashi@fujielab.org>
2022-11-15 15:45:54 +0900

    顔方向推定を追加

--- kanjou/emo_feat.py
+++ kanjou/emo_feat.py
@@ -1,4 +1,5 @@
 ## py-featを使用したリアルタイム顔感情認識
+## 顔方向認識
 import cv2
 import numpy as np
 from feat import Detector
@@ -27,15 +28,27 @@ emolist = ["anger", "disgust", "fear", "happiness", "sadness", "surprise", "neut
 while True:
     #画像取得
     ret, img = cap.read()
-    show_img = cv2.resize(img, (frameWidth, frameHeight))
-    cv2.imshow('Video', show_img)
+    img = cv2.resize(img, (frameWidth, frameHeight))
+    cv2.imshow('Video', img)
     img = np.expand_dims(img,0)
 
-    #特徴抽出，感情推定
+    #特徴抽出，感情推定，顔方向推定
     faces = detector.detect_faces(img)
+
+    # 顔検出判定
+    if faces == [[]]:
+        print("No faces found")
+        continue
+
     landmarks = detector.detect_landmarks(img, faces)
     emo_pred = detector.emotion_model.detect_emo(img, landmarks)
+    poses = detector.detect_facepose(img, faces, landmarks)
     
     #結果表示
     pred_index = np.argmax(emo_pred[0])
     print(emolist[pred_index])
+
+    #下向きが負[deg]
+    print("pitch", poses[0][0][0][0])
+    # 左向きが負[deg]
+    print("yaw", poses[0][0][0][2])
